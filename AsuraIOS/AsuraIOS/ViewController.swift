fb229d15ae7017f47acac5e6d2acad0dcc233518
David Lin <lindavid@davids-imac.gateway.pace.com>
2016-01-14 21:22:28 -0800

    connect with Parse

---
+++ AsuraIOS/AsuraIOS/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  AsuraIOS
+//
+//  Created by David Lin on 1/14/16.
+//  Copyright © 2016 David Lin. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //let testObject = PFObject(className: "TestObject")
+        //testObject["foo"] = "bar"
+        //testObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+        //    print("Object has been saved.")
+        //}
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
